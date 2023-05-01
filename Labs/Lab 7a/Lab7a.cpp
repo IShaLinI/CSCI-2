623eb0b614b27d24dcbc81ec8e851d3b791fdfa7
IShaLinI <mrsaltinecracker1@gmail.com>
2023-05-01 16:44:10 -0700

    Lab 7b

--- Labs/Lab 7a/Lab7a.cpp
+++ Labs/Lab 7a/Lab7a.cpp
@@ -5,14 +5,6 @@
 
 using namespace std;
 
-int stringToIndexHash(string userName, int buckets){
-    int sum = 0;
-    for(int i = 0; i < userName.length(); i++){
-        sum += userName[i];
-    }
-    return sum % buckets;
-}
-
 string lowercase(string input){
     string output = "";
     for(int i = 0; i < input.length(); i++){
@@ -25,26 +17,147 @@ string lowercase(string input){
     return output;
 }
 
-int main(){
+int stringToIndexHash(string input, int buckets){
 
-    //Get user string input.
-    string input;
-    cout << "Enter a string: ";
-    getline(cin,input);
-
-    //Get number of buckets.
-    int buckets;
-    cout << "Enter the number of buckets: ";
-    cin >> buckets;
-
-    //Convert to lowercase
     input = lowercase(input);
 
-    //Hash the string
-    int index = stringToIndexHash(input, buckets);
+    int sum = 0;
+    for(int i = 0; i < input.length(); i++){
+        sum += input[i];
+    }
+    return sum % buckets;
+}
 
-    //Output the index
-    cout << "Index: " << index << endl;
+//Hash Table Item
+class HashTableItem{
+    private:
+        string key;
+        int value;
+    public:
+        HashTableItem(string key, int value){
+            this->key = key;
+            this->value = value;
+        }
+        string getKey(){
+            return key;
+        }
+        int getValue(){
+            return value;
+        }
+};
+
+class HashTable {
+    private:
+        int size;
+        HashTableItem** table;
+    public:
+        HashTable(int size){
+            this->size = size;
+            table = new HashTableItem*[size];
+            for(int i = 0; i < size; i++){
+                table[i] = NULL;
+            }
+        }
+        void insert(string key, int value){
+            int index = stringToIndexHash(key, size);
+            while(table[index] != NULL){
+                index = (index + 1) % size;
+            }
+            table[index] = new HashTableItem(key, value);
+        }
+        void print(){
+            for(int i = 0; i < size; i++){
+                if(table[i] != NULL){
+                    cout << table[i]->getKey() << " " << table[i]->getValue() << endl;
+                }
+            }
+        }
+        //Get Size
+        int getSize(){
+            return size;
+        }
+        
+        //Get Table
+        HashTableItem** getTable(){
+            return table;
+        }
+};
+
+//Sort hashtable
+void sort(HashTable* table){
+
+    //Create a new hash table
+    HashTable* newTable = new HashTable(table->getSize());
+
+    //Loop through the old table
+    for(int i = 0; i < table->getSize(); i++){
+
+        //If the item is not null
+        if(table->getTable()[i] != NULL){
+
+            //Insert into the new table
+            newTable->insert(table->getTable()[i]->getKey(), table->getTable()[i]->getValue());
+
+        }
+
+    }
+
+    //Delete the old table
+    delete table;
+
+    //Set the old table to the new table
+    table = newTable;
+
+}
+
+
+
+int main(){
+
+    //Create a hash table
+    HashTable* table = new HashTable(20);
+
+    while(true){
+
+        system("CLS");
+
+        //Get user string input.
+        string name;
+        cout << "Enter your name: ";
+        getline(cin, name);
+
+        //Get user's favorite number.
+        int number;
+        cout << "Enter your favorite number: ";
+        cin >> number;
+
+        //Insert into hash table
+        table->insert(name, number);
+
+        //Ask to run again
+        cout << "More names? (y/n): ";
+        char runAgain;
+        cin >> runAgain;
+
+        if(runAgain == 'n'){
+            break;
+        }
+
+        cin.ignore();
+
+    }
+
+    //Print the hash table
+    system("CLS");
+    cout << "Unsorted Hash Table" << endl;
+    table->print();
+
+    //Sort the hash table
+    sort(table);
+
+    //Print the hash table
+    cout << endl << "Sorted Hash Table" << endl;
+    table->print();
 
     return 0;
 }
---
+++ Labs/Lab 7b/Lab7b.cpp
@@ -0,0 +1,85 @@
+//Andrew Card
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <list>
+using namespace std;
+
+//Add up chars in string and mod by table size
+int hashFunction(string ID, int tableSize) {
+
+    //Convert to lowercase
+    for (int i = 0; i < ID.length(); i++) {
+        if (ID[i] >= 'A' && ID[i] <= 'Z') {
+            ID[i] += 32;
+        }
+    }
+
+    //Add up chars
+    int sum = 0;
+    for (int i = 0; i < ID.length(); i++) {
+        sum += ID[i];
+    }
+    
+    return sum % tableSize;
+}
+
+void simplePrintTable(list<string>[], int); //pass array and size
+
+int main(void) {
+	srand(time(0));
+	const int tableSize = 3;
+
+	// A c-type ARRAY of integer lists -- Want the indexing here.
+	list<string> hashTable[tableSize];
+
+    while(true){
+        system("CLS");
+	    
+        //Take User String
+        string input;
+        cout << "Enter a string: ";
+        getline(cin, input);
+
+        //Hash the string
+        int index = hashFunction(input, tableSize);
+
+        //Add the string to the hash table
+        hashTable[index].push_back(input);
+        
+        //print the hash table
+	    simplePrintTable(hashTable, tableSize);
+
+        //Ask user if they want to continue
+        cout << "Continue? (y/n): ";
+        char runAgain;
+        cin >> runAgain;
+
+        if(runAgain == 'n'){
+            break;
+        }
+
+        cin.ignore();
+
+    }
+	return(0);
+}
+
+//Implementations
+void simplePrintTable(list<string> arg[], int size) {
+	// Print the buckets...
+	list<string>::iterator myItr;
+
+	for (int i = 0; i < size; i++) {
+		cout << "bucket " << i << " ==> ";
+		myItr = arg[i].begin();
+
+		while (myItr != arg[i].end()) {
+			cout << *myItr << ", ";
+			myItr++;
+		}
+		cout << endl;
+	}
+}
